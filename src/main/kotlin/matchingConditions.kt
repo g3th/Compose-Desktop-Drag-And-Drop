deb18114254b43159c34e11093d690efac982402
g3th <theinkandpen@yahoo.co.uk>
2024-05-09 23:48:19 +0100

    fixed: clearer argument names, instead of obscure concise names

--- src/main/kotlin/matchingConditions.kt
+++ src/main/kotlin/matchingConditions.kt
@@ -5,12 +5,12 @@ import androidx.compose.ui.graphics.Color
 data class CollisionValues(val mState: Boolean, val mText: Color, val hasCollided: Boolean)
 class Collisions {
 
-  fun detect(dObj: Offset, tar: Offset): CollisionValues {
+  fun detect(dragObjectOffset: Offset, dragListenerOffset: Offset): CollisionValues {
     val matchText: Color
     val matchState: Boolean
     var hasCollided: Boolean
-    if (dObj.x in tar.x - 10 .. tar.x + 10 &&
-      dObj.y in tar.y - 10 .. tar.y + 10) {
+    if (dragObjectOffset.x in dragListenerOffset.x - 10 .. dragListenerOffset.x + 10 &&
+      dragObjectOffset.y in dragListenerOffset.y - 10 .. dragListenerOffset.y + 10) {
       matchText = Color.Red
       matchState = true
       hasCollided = true
